d8eccfa78203d25c069111dcd4a2065f6b1851fa
maxwbuckley <maxwbuckley@gmail.com>
2015-11-07 00:16:56 +0000

    Restructured and seperated functions int library

---
+++ lib/mcda_lib.py
@@ -0,0 +1,69 @@
+
+import json
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
+import numpy
+import pandas
+
+def getDataFromGoogleSpreadsheet():
+  json_key = json.load(open('MCDA-UCD-8b0b964e491d.json'))
+  scope = ['https://spreadsheets.google.com/feeds']
+
+  credentials = SignedJwtAssertionCredentials(
+      json_key['client_email'], json_key['private_key'].encode(), scope)
+
+  gc = gspread.authorize(credentials)
+  wks = gc.open_by_key(
+      "13KcEqiK6tq2Vv72jK4np0kIAq2COlRVy1vPe_5DDrCk").get_worksheet(0)
+
+  cell_list = wks.get_all_records()
+  return cell_list
+
+
+def getStructuredDataRollup(data):
+  return_list = []
+  split_keys = set()
+  for data_dict in data:
+    map_values = {}
+    for key, value in data_dict.iteritems():
+      if key != "Timestamp":
+        keyword1, keyword2 = key.split(' or ')
+        # This is where I assign the scores that ends up in the final matrices.
+        map_values[key] =  {keyword1: 8-value, keyword2: value}
+        split_keys = split_keys.union(set([keyword1, keyword2]))
+    return_list.append(map_values)
+  return return_list, split_keys
+
+
+def ConstructQuestionScoringMatrix(question_set, map_structure):
+  n = len(question_set)
+  question_list = sorted([question for question in question_set])
+  zero_mat = numpy.zeros((n,n))
+  df = pandas.DataFrame(zero_mat, index=question_list, columns=question_list)
+  for row in map_structure:
+    for key, value in row.iteritems():
+      # key is the question and value is a dict of keyword to score.
+      keyword1, keyword2 = key.split(' or ')
+      # Just need to check the first of the or words
+      if keyword1 in question_set:
+        df[keyword1][keyword2] += value[keyword2]
+        df[keyword2][keyword1] += value[keyword1]
+  return df
+
+
+def NormalizeDataFrame(data_frame):
+  new_df = data_frame /data_frame.sum()
+  return new_df
+
+
+
+def GetMarkovScoresList(QuestionSquareMatrix):
+  NormalizedSquareMatrix = NormalizeDataFrame(QuestionSquareMatrix)
+  n = len(NormalizedSquareMatrix)
+  transition_matrix = numpy.linalg.matrix_power(NormalizedSquareMatrix, 1000)
+  # Need an n * 1 matrix of 1/n to multiply by the transition matrix
+  fractional_score_matrix = numpy.ones((n,1))*(1/(n*1.0))
+  final_score = numpy.dot(transition_matrix, fractional_score_matrix)
+  labels = list(NormalizedSquareMatrix.index)
+  output = zip(labels, final_score)
+  return output
--- mcda.py
+++ mcda.py
@@ -3,8 +3,9 @@
 import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
-import numpy as np
-import pandas as pd
+import numpy
+import pandas
+import lib.mcda_lib as mcda_lib
 
 TOPLEVEL = set(["'Will they like it?'", "'Will they be good at it?'", 
                 "'Can we afford them?'"])
@@ -30,83 +31,22 @@ ENOUGHRESPONSIBILITY = set([ "Seniority" ,"Reportees"])
 QUESTIONS = [TOPLEVEL, WILLTHEYLIKEIT, WILLTHEYBEGOODAIT, CANWEAFFORDTHEM,
              TECHNICALSKILLS, EXPERIENCE, EDUCATION, ENOUGHRESPONSIBILITY]
 
-def getDataFromGoogleSpreadsheet():
-  json_key = json.load(open('MCDA-UCD-8b0b964e491d.json'))
-  scope = ['https://spreadsheets.google.com/feeds']
-
-  credentials = SignedJwtAssertionCredentials(
-      json_key['client_email'], json_key['private_key'].encode(), scope)
-
-  gc = gspread.authorize(credentials)
-  wks = gc.open_by_key(
-      "13KcEqiK6tq2Vv72jK4np0kIAq2COlRVy1vPe_5DDrCk").get_worksheet(0)
-
-  cell_list = wks.get_all_records()
-  return cell_list
-
-def getRelevantQuestionSet(value):
-  for question_set in QUESTIONS:
-    if value in question_set:
-      return question_set
 
 
-def getStructuredDataRollup(data):
-  return_list = []
-  split_keys = set()
-  for data_dict in data:
-    map_values = {}
-    for key, value in data_dict.iteritems():
-      if key != "Timestamp":
-        keyword1, keyword2 = key.split(' or ')
-        # This is where I assign the scores that ends up in the final matrices.
-        map_values[key] =  {keyword1: 8-value, keyword2: value}
-        split_keys = split_keys.union(set([keyword1, keyword2]))
-    return_list.append(map_values)
-  return return_list, split_keys
+def main():
+  data = mcda_lib.getDataFromGoogleSpreadsheet()
+  map_structure, keys = mcda_lib.getStructuredDataRollup(data)
 
-def ConstructQuestionScoringMatrix(question_set, map_structure):
-  n = len(question_set)
-  question_list = sorted([question for question in question_set])
-  zero_mat = np.zeros((n,n))
-  df = pd.DataFrame(zero_mat, index=question_list, columns=question_list)
-  for row in map_structure:
-    for key, value in row.iteritems():
-      # key is the question and value is a dict of keyword to score.
-      keyword1, keyword2 = key.split(' or ')
-      # Just need to check the first of the or words
-      if keyword1 in question_set:
-        df[keyword1][keyword2] += value[keyword2]
-        df[keyword2][keyword1] += value[keyword1]
-
-  return df
+  dataframe = mcda_lib.ConstructQuestionScoringMatrix(TOPLEVEL, map_structure)
+  print dataframe
 
 
-def NormalizeDataFrame(data_frame):
-  new_df = data_frame /data_frame.sum()
-  return new_df
+  norm_dataframe = mcda_lib.NormalizeDataFrame(dataframe)
+  print norm_dataframe
 
 
+  final_scores = mcda_lib.GetMarkovScoresList(norm_dataframe)
+  print final_scores
 
-def getMarkovScores(NormalizedSquareMatrix):
-  n = len(NormalizedSquareMatrix)
-  transition_matrix = np.linalg.matrix_power(NormalizedSquareMatrix, 1000)
-  # Need an n * 1 matrix of 1/n to multiply by the transition matrix
-  fractional_score_matrix = np.ones((n,1))*(1/(n*1.0))
-  final_score = np.dot(transition_matrix, fractional_score_matrix)
-  labels = list(new_df.index)
-  output = zip(labels, final_score)
-  return output
-
-data = getDataFromGoogleSpreadsheet()
-map_structure, keys = getStructuredDataRollup(data)
-#print map_structure
-
-dataframe = ConstructQuestionScoringMatrix(TOPLEVEL, map_structure)
-print dataframe
-
-
-norm_dataframe = NormalizeDataFrame(dataframe)
-print norm_dataframe
-
-
-
+if __name__ == "__main__":  
+  main()
