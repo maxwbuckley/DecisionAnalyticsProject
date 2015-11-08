3b6b6661a5d23d951a639273bac64a4aa44ca708
maxwbuckley <maxwbuckley@gmail.com>
2015-11-08 22:58:39 +0000

    Updated style and rearranged more code into the library and out of main.

--- mcda.py
+++ mcda.py
@@ -1,63 +1,60 @@
 #! /usr/bin/python
+"""Reads and preps data from spreadsheet. Generates scores and plots them.
+""" 
 
-import matplotlib.pyplot as plt
-import networkx as nx
+import sys
 import lib.mcda_lib as mcda_lib
 
-TOPLEVEL = set(["'Will they like it?'", "'Will they be good at it?'", 
-                "'Can we afford them?'"])
 
-WILLTHEYBEGOODATIT = set(["Soft Skills", "Technical Skills", "Experience",
-                         "Education"])
+TOPLEVEL = set(['Will they like it?', 'Will they be good at it?', 
+                'Can we afford them?'])
 
-TECHNICALSKILLS = set(["Programming", "Databases", "Analytics/Statistics"])
+WILLTHEYBEGOODATIT = set(['Soft Skills', 'Technical Skills', 'Experience',
+                         'Education'])
 
-EXPERIENCE = set(["Industry specificity", "Years of experience", 
-                  "Job title relevance"])
+TECHNICALSKILLS = set(['Programming', 'Databases', 'Analytics/Statistics'])
 
-EDUCATION = set(["School/College Name", "Strong Grades", "Course Relevance",
-                 "Qualification Level (BSc/MSc/Phd)"])
+EXPERIENCE = set(['Industry specificity', 'Years of experience', 
+                  'Job title relevance'])
 
-WILLTHEYLIKEIT = set(["Cultural fit", "Career alignment/development",
-                      "Personal development goals"])
+EDUCATION = set(['School/College Name', 'Strong Grades', 'Course Relevance',
+                 'Qualification Level (BSc/MSc/Phd)'])
 
-CANWEAFFORDTHEM = set(["Monetary Price", "Enough Responsibility"])
+WILLTHEYLIKEIT = set(['Cultural fit', 'Career alignment/development',
+                      'Personal development goals'])
 
-ENOUGHRESPONSIBILITY = set([ "Seniority" ,"Reportees"])
+CANWEAFFORDTHEM = set(['Monetary Price', 'Enough Responsibility'])
 
-QUESTIONS_DICT = {'ROOT' : TOPLEVEL, '\'Will they like it?\'' : WILLTHEYLIKEIT,
-                  "'Will they be good at it?'" : WILLTHEYBEGOODATIT,
-                  "'Can we afford them?'": CANWEAFFORDTHEM, "Technical Skills" :
-                  TECHNICALSKILLS, "Education": EDUCATION, "Experience": EXPERIENCE,
-                  "Enough Responsibility" : ENOUGHRESPONSIBILITY}
+ENOUGHRESPONSIBILITY = set([ 'Seniority' ,'Reportees'])
+
+QUESTIONS_DICT = {'ROOT': TOPLEVEL,
+                  'Will they like it?': WILLTHEYLIKEIT,
+                  'Will they be good at it?': WILLTHEYBEGOODATIT,
+                  'Can we afford them?': CANWEAFFORDTHEM, 
+                  'Technical Skills': TECHNICALSKILLS,
+                  'Education': EDUCATION,
+                  'Experience': EXPERIENCE,
+                  'Enough Responsibility': ENOUGHRESPONSIBILITY}
 
 
 def main():
   scoring_default = False
-  data = mcda_lib.getDataFromGoogleSpreadsheet()
-  structured_data_dict, keys = mcda_lib.getStructuredDataRollup(data, scoring_default)
-  all_edges = mcda_lib.GenerateCompleteGraph(QUESTIONS_DICT,
-                                             structured_data_dict, scoring_default)
-  graph = nx.DiGraph()
-  for edge in all_edges:
-    graph.add_edge(edge.source, edge.target, weight=edge.relative_weight)
+  #TODO(Max): Add a bias term for the alternative scoring system.
+  data = mcda_lib.GetDataFromGoogleSpreadsheet(
+      '13KcEqiK6tq2Vv72jK4np0kIAq2COlRVy1vPe_5DDrCk')
+  structured_data_dict = mcda_lib.GetStructuredDataRollup(data, scoring_default)
+  graph = mcda_lib.GenerateNetworkGraph(
+      QUESTIONS_DICT, structured_data_dict, scoring_default)
 
-  pos=nx.graphviz_layout(graph, prog='dot')
-  nx.draw_networkx(graph, pos=pos, ax=None, with_labels=True, font_size=7)
-  labels = nx.get_edge_attributes(graph,'weight')
-  nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels, font_size=7)
-
-  plt.draw()
-  #plt.savefig("graph.png", dpi=1000)
-  #plt.show()
-  print '\n\n'
-  weights = mcda_lib.getEndLevelWeights(graph)
-  cumulative_sum = 0
+  print '\n',
+  weights = mcda_lib.GetEndLevelWeights(graph)
   for label, weight in weights:
     print label, weight
-    cumulative_sum += weight
-  print cumulative_sum
-  #print graph.neighbors('Cultural fit')
+  print '\n',
+  if len(sys.argv)>1:
+    if sys.argv[1] == 'plot':
+      mcda_lib.PlotNetworkGraph(graph)
 
-if __name__ == "__main__":  
+
+if __name__ == '__main__':  
   main()
